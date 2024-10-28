bd3e345512f99a75ba0e65553bcca25e9cba217f
tcguus <gugucram15@gmail.com>
2024-10-28 09:34:22 -0300

    Estilizando a seção 4 e aplicando o modo noturno.

--- sprint4/src/app/page.tsx
+++ sprint4/src/app/page.tsx
@@ -1,8 +1,11 @@
 "use client"
 
+import Image from 'next/image';
 import { useState } from 'react';
 import { FaBalanceScale, FaTools } from "react-icons/fa";
 import { FaAngleDown, FaGears } from "react-icons/fa6";
+import { MdOutlinePlace } from "react-icons/md";
+import Link from "next/link";
 
 export default function Home() {
   const [accordion, setAccordion] = useState(0);
@@ -247,7 +250,198 @@ export default function Home() {
             </form>
           </div>
         </div>
-        <div id="sec4" className="w-full bg-green-500 min-h-[calc(100vh-3rem)] scroll-mt-[3rem]">sec4</div>
+        <div id="sec4" className="w-full bg-white dark:bg-preto flex min-h-[calc(100vh-3rem)] scroll-mt-[3rem]">
+          <div className='w-[50%] flex flex-col justify-center items-center min-h-[calc(100vh-3rem)]'>
+            <h1 className='font-bold text-3xl text-center'>Encontre a Oficina da Porto mais próxima de você!</h1>
+            <Image
+              src="/loc-branco.svg"
+              alt="Localização"
+              width={600}
+              height={300}
+              className="mt-[1.5rem]"
+            />
+          </div>
+          <div className='w-[50%] flex justify-center items-center'>
+            <div className="space-y-6 overflow-auto overflow-y-scroll items-center w-[60%] h-[40rem] flex-grow flex flex-col m-auto">
+              <div className="p-4 bg-gray-100 dark:bg-cinzaescuro w-[60%] rounded shadow">
+                <h3 className="text-lg font-semibold">Reycar</h3>
+                <p className='flex'>
+                  <MdOutlinePlace  className="fa-solid fa-location-dot mr-2 mt-1" />
+                  Rua LUIS GAMA 185 - MOOCA - SÃO PAULO/SP
+                  <br /> (11) 3209-1166
+                </p>
+                <Link
+                  href="https://www.google.com/search?q=Reycar"
+                  target="_blank"
+                  className="text-blue-500 hover:underline"
+                >
+                  Ver Detalhes
+                </Link>
+              </div>
+
+              <div className="p-4 bg-gray-100 dark:bg-cinzaescuro rounded w-[60%] shadow">
+                <h3 className="text-lg font-semibold">Canaã Funilaria e Pintura</h3>
+                <p className='flex'>
+                  <MdOutlinePlace  className="fa-solid fa-location-dot mr-2 mt-1" />
+                  Rua CONSELHEIRO RAMALHO 487 - BELA VISTA - SÃO PAULO/SP
+                  <br /> (11) 3104-7760
+                </p>
+                <Link
+                  href="https://www.google.com/search?q=Canaa+Funelaria+e+Pintura"
+                  target="_blank"
+                  className="text-blue-500 hover:underline"
+                >
+                  Ver Detalhes
+                </Link>
+              </div>
+
+              <div className="p-4 bg-gray-100 dark:bg-cinzaescuro rounded w-[60%] shadow">
+                <h3 className="text-lg font-semibold">Oficina RN Service Car</h3>
+                <p className='flex'>
+                  <MdOutlinePlace  className="fa-solid fa-location-dot mr-2 mt-1" />
+                  Rua HELVETIA 468 - CAMPOS ELISEOS - SÃO PAULO/SP
+                  <br /> (11) 3361-8677
+                </p>
+                <Link
+                  href="https://www.google.com/search?q=Oficina+RN+Service+Car"
+                  target="_blank"
+                  className="text-blue-500 hover:underline"
+                >
+                  Ver Detalhes
+                </Link>
+              </div>
+
+              <div className="p-4 bg-gray-100 dark:bg-cinzaescuro rounded w-[60%] shadow">
+                <h3 className="text-lg font-semibold">Stop Car Bem</h3>
+                <p className='flex'>
+                  <MdOutlinePlace  className="fa-solid fa-location-dot mr-2 mt-1" />
+                  Rua DOUTOR FREIRE 192 - BRÁS - SÃO PAULO/SP
+                  <br /> (11) 93004-0738
+                </p>
+                <Link
+                  href="https://www.google.com/search?q=Stop+Car+Bem"
+                  target="_blank"
+                  className="text-blue-500 hover:underline"
+                >
+                  Ver Detalhes
+                </Link>
+              </div>
+
+              <div className="p-4 bg-gray-100 dark:bg-cinzaescuro rounded w-[60%] shadow">
+                <h3 className="text-lg font-semibold">Clubcar Veículos Cambuci</h3>
+                <p className='flex'>
+                  <MdOutlinePlace  className="fa-solid fa-location-dot mr-2 mt-1" />
+                  Rua CLIMACO BARBOSA 191 - CAMBUCI - SÃO PAULO/SP
+                  <br /> (11) 3346-2582
+                </p>
+                <Link
+                  href="https://www.google.com/search?q=Clubcar+Veiculos+Cambuci"
+                  target="_blank"
+                  className="text-blue-500 hover:underline"
+                >
+                  Ver Detalhes
+                </Link>
+              </div>
+
+              <div className="p-4 bg-gray-100 dark:bg-cinzaescuro rounded w-[60%] shadow">
+                <h3 className="text-lg font-semibold">Ribeiro Renovadora de Veículos</h3>
+                <p className='flex'>
+                  <MdOutlinePlace  className="fa-solid fa-location-dot mr-2 mt-1" />
+                  Rua JOSE BENTO 50 - CAMBUCI - SÃO PAULO/SP
+                  <br /> (11) 3209-2954
+                </p>
+                <Link
+                  href="https://www.google.com/search?q=Ribeiro+Renovadora+de+Veículos"
+                  target="_blank"
+                  className="text-blue-500 hover:underline"
+                >
+                  Ver Detalhes
+                </Link>
+              </div>
+
+              <div className="p-4 bg-gray-100 dark:bg-cinzaescuro rounded w-[60%] shadow">
+                <h3 className="text-lg font-semibold">Quality AutoTec</h3>
+                <p className='flex'>
+                  <MdOutlinePlace  className="fa-solid fa-location-dot mr-2 mt-1" />
+                  Rua SALVADOR LEME 291 - BOM RETIRO - SÃO PAULO/SP
+                  <br /> (11) 3326-9947
+                </p>
+                <Link
+                  href="https://www.google.com/search?q=Quality+AutoTec"
+                  target="_blank"
+                  className="text-blue-500 hover:underline"
+                >
+                  Ver Detalhes
+                </Link>
+              </div>
+
+              <div className="p-4 bg-gray-100 dark:bg-cinzaescuro rounded w-[60%] shadow">
+                <h3 className="text-lg font-semibold">Trend Auto Service</h3>
+                <p className='flex'>
+                  <MdOutlinePlace  className="fa-solid fa-location-dot mr-2 mt-1" />
+                  Rua CONSELHEIRO NÉBIAS 1530 - CAMPOS ELISEOS - SÃO PAULO/SP
+                  <br /> (11) 99675-1794
+                </p>
+                <Link
+                  href="https://www.google.com/search?q=Trend+Studio+oficina"
+                  target="_blank"
+                  className="text-blue-500 hover:underline"
+                >
+                  Ver Detalhes
+                </Link>
+              </div>
+
+              <div className="p-4 bg-gray-100 dark:bg-cinzaescuro rounded w-[60%] shadow">
+                <h3 className="text-lg font-semibold">Oficina Globo</h3>
+                <p className='flex'>
+                  <MdOutlinePlace  className="fa-solid fa-location-dot mr-2 mt-1" />
+                  Estrada PRT 163 S/N - ZONA RURAL - CAPITÃO LEÔNIDAS MARQUES/PR
+                  <br /> (45) 3286-1419
+                </p>
+                <Link
+                  href="https://www.google.com/search?q=Oficina+Globo"
+                  target="_blank"
+                  className="text-blue-500 hover:underline"
+                >
+                  Ver Detalhes
+                </Link>
+              </div>
+
+              <div className="p-4 bg-gray-100 dark:bg-cinzaescuro rounded w-[60%] shadow">
+                <h3 className="text-lg font-semibold">Nicola Garage Serviços Automotivos</h3>
+                <p className='flex'>
+                  <MdOutlinePlace  className="fa-solid fa-location-dot mr-2 mt-1" />
+                  Rua BARRA FUNDA 308 - BARRA FUNDA - SÃO PAULO/SP
+                  <br /> (11) 3666-6655
+                </p>
+                <Link
+                  href="https://www.google.com/search?q=Nicola+Garage+Serviços+Automotivos"
+                  target="_blank"
+                  className="text-blue-500 hover:underline"
+                >
+                  Ver Detalhes
+                </Link>
+              </div>
+
+              <div className="p-4 bg-gray-100 dark:bg-cinzaescuro rounded w-[60%] shadow">
+                <h3 className="text-lg font-semibold">Oficina 4 Rodas Mais - Funilaria Express</h3>
+                <p className='flex'>
+                  <MdOutlinePlace  className="fa-solid fa-location-dot mr-2 mt-1" />
+                  Rua ORVILLE DERBY 140 - MOOCA - SÃO PAULO/SP
+                  <br /> (11) 2697-3057
+                </p>
+                <Link
+                  href="https://www.google.com/search?q=Oficina+4+Rodas+Mais+-+Funilaria+Express"
+                  target="_blank"
+                  className="text-blue-500 hover:underline"
+                >
+                  Ver Detalhes
+                </Link>
+              </div>
+            </div>
+
+          </div>
+        </div>
         <div id="sec5" className="w-full min-h-[calc(100vh-3rem)] scroll-mt-[3rem]">sec5</div>
       </div>
     </main>
--- sprint4/src/components/Footer/Footer.tsx
+++ sprint4/src/components/Footer/Footer.tsx
@@ -7,70 +7,71 @@ import { IoLogoWhatsapp } from "react-icons/io";
 import { FaMapLocationDot } from "react-icons/fa6";
 import { IoIosAppstore } from "react-icons/io";
 import { BiLogoPlayStore } from "react-icons/bi";
+import Link from "next/link";
 
 export default function Footer() {
   return (
     <footer className="w-full h-[6rem] bg-cinzaescuro text-white text-lg flex justify-between">
       <div className="w-[33vw] flex flex-col justify-center gap-2 pl-[1rem]">
-        <a
+        <Link
           href="https://api.whatsapp.com/send?1=pt_BR&phone=551130039303&text=Oi,%20Porto%20Seguro%20!"
           target="_blank"
           rel="noopener noreferrer"
           className="flex self-start text-[1.1rem] items-center gap-1 transition duration-300 hover:text-azul"
         >
           <IoLogoWhatsapp /> WhatsApp
-        </a>
-        <a
+        </Link>
+        <Link
           href="https://www.google.com/search?q=Agencia+Porto+Seguro+mais+próxima+de+mim&client=opera-gx&sca_esv=0ab730e59f55f214&sxsrf=ADLYWIIzKKUdF_NpdaCU-W4iJC5wNsj43g%3A1715041360863&ei=UHQ5ZtGhNNOp5OUPx6uB2AQ&udm=&ved=0ahUKEwjRidv4ovqFAxXTFLkGHcdVAEsQ4dUDCBA&uact=5&oq=Agencia+Porto+Seguro+mais+próxima+de+mim&gs_lp=Egxnd3Mtd2l6LXNlcnAiKUFnZW5jaWEgUG9ydG8gU2VndXJvIG1haXMgcHLDs3hpbWEgZGUgbWltMggQABiABBiiBDIIEAAYgAQYogQyCBAAGIAEGKIESJgOUJUHWJYLcAF4AZABAJgBpgGgAbICqgEDMC4yuAEDyAEA-AEBmAIDoAK6AsICChAAGLADGNYEGEfCAgQQIRgKmAMAiAYBkAYIkgcDMS4yoAfSBQ&sclient=gws-wiz-serp"
           target="_blank"
           rel="noopener noreferrer"
           className="flex self-start text-[1.1rem] items-center gap-1 transition duration-300 hover:text-azul"
         >
           <FaMapLocationDot /> Âgencia mais próxima
-        </a>
+        </Link>
       </div>
       <div className="w-[33vw] flex flex-col">
         <div className="h-[50%] w-full flex justify-evenly">
-          <a
+          <Link
             href="https://www.instagram.com/portoseguro/"
             target="_blank"
             rel="noopener noreferrer"
             className="mt-auto transition duration-300 hover:text-azul"
           >
             <AiFillInstagram className="text-3xl" />
-          </a>
-          <a
+          </Link>
+          <Link
             href="https://www.tiktok.com/@portoseguro"
             target="_blank"
             rel="noopener noreferrer"
             className="mt-auto transition duration-300 hover:text-azul"
           >
             <AiFillTikTok className="text-3xl" />
-          </a>
-          <a
+          </Link>
+          <Link
             href="https://www.youtube.com/portoseguro"
             target="_blank"
             rel="noopener noreferrer"
             className="mt-auto transition duration-300 hover:text-azul"
           >
             <FaYoutube className="text-3xl" />
-          </a>
-          <a
+          </Link>
+          <Link
             href="https://www.facebook.com/porto"
             target="_blank"
             rel="noopener noreferrer"
             className="mt-auto transition duration-300 hover:text-azul"
           >
             <FaFacebookSquare className="text-3xl" />
-          </a>
-          <a
+          </Link>
+          <Link
             href="https://www.linkedin.com/company/porto/"
             target="_blank"
             rel="noopener noreferrer"
             className="mt-auto transition duration-300 hover:text-azul"
           >
             <FaLinkedin className="text-3xl" />
-          </a>
+          </Link>
         </div>
         <div className="w-full h-[50%] flex justify-center">
           <p className="text-sm font-medium mt-[auto] mb-[0.2rem]">
@@ -82,12 +83,12 @@ export default function Footer() {
         <div className="flex flex-col items-center pr-[1rem] gap-2">
           <p className="">Baixe o app da Porto!</p>
           <div className="flex gap-8 text-right items-center">
-            <a href="https://apps.apple.com/br/app/porto-seguros-cartão-e/id1511026277" target="_blank" rel="noopener noreferrer" className="transition duration-300 hover:text-azul">
+            <Link href="https://apps.apple.com/br/app/porto-seguros-cartão-e/id1511026277" target="_blank" rel="noopener noreferrer" className="transition duration-300 hover:text-azul">
               <IoIosAppstore className="text-3xl" />
-            </a>
-            <a href="https://play.google.com/store/apps/details?id=br.com.portoseguro.experienciacliente.mundoporto&pcampaignid=web_share" target="_blank" rel="noopener noreferrer" className="transition duration-300 hover:text-azul">
+            </Link>
+            <Link href="https://play.google.com/store/apps/details?id=br.com.portoseguro.experienciacliente.mundoporto&pcampaignid=web_share" target="_blank" rel="noopener noreferrer" className="transition duration-300 hover:text-azul">
               <BiLogoPlayStore className="text-3xl"/>
-            </a>
+            </Link>
           </div>
         </div>
       </div>
