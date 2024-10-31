2672379cbbb98c5053cb14c46e00d502a8a6f8ba
leoc7sar <lcrn1102@gmail.com>
2024-10-31 16:27:43 -0300

    Criando a página de cadastro, aplicando o modo noturno e fazendo alguns ajustes no projeto.

---
+++ sprint4/src/app/cadastro/page.tsx
@@ -0,0 +1,105 @@
+"use client";
+ 
+import { IoMdCloseCircleOutline } from "react-icons/io";
+import Image from "next/image";
+import ThemeToggle from "@/components/ThemeToggle";
+import { useState } from "react";
+import Link from "next/link";
+import { FaArrowLeft } from "react-icons/fa";
+ 
+export default function Login() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const handleThemeToggle = (darkMode: boolean) => {
+    setIsDarkMode(darkMode);
+  };
+ 
+  return (
+    <div className="flex w-full h-[100vh] justify-evenly items-center transition duration-300 dark:bg-cinzaescuro">
+      <div className="flex gap-2">
+        <Image
+          src="/logo-porto.svg"
+          width="150"
+          height="150"
+          alt="Logo Porto Seguro"
+        />
+        <div className="flex flex-col justify-center gap-2">
+          <p className="font-bold text-azul text-[4rem] leading-none">
+            Pygital
+          </p>
+          <p className="font-bold text-azul text-[4rem] leading-none">Porto</p>
+        </div>
+      </div>
+      <div className="w-[35rem] h-[40rem] bg-cinza flex flex-col border-4 rounded-xl border-azul dark:bg-preto">
+        <div className="flex w-full h-[3rem] justify-end items-center pr-4 gap-2 text-azul">
+            <Link className="mr-auto ml-3" href="/login"><FaArrowLeft className="text-[1.4rem]" /></Link>
+          <ThemeToggle onToggle={handleThemeToggle} />
+          <Link href="/">
+            <IoMdCloseCircleOutline className="text-[1.7rem] hover:text-red-500 transtion duration-300" />
+          </Link>
+        </div>
+        <form className="w-full h-[35rem] flex flex-col gap-2 justify-center items-center">
+          <h1 className="font-bold text-3xl">Cadastre-se</h1>
+          <p className="text">Preencha os dados:</p>
+          <div className="flex flex-col gap-1">
+            <label htmlFor="nome" className="font-bold ml-1">
+              Nome:
+            </label>
+            <input
+              type="text"
+              placeholder="Digite seu nome completo..."
+              className="placeholder-black focus:outline-none dark:bg-cinzaescuro dark:placeholder-white p-2 border-2 border-azul rounded-xl placeholder:text-sm"
+              required
+              autoComplete="off"
+            />
+          </div>
+          <div className="flex flex-col gap-1">
+            <label htmlFor="cpf" className="font-bold ml-1">
+              CPF:
+            </label>
+            <input
+              type="text"
+              placeholder="Digite seu CPF..."
+              className="placeholder-black focus:outline-none dark:bg-cinzaescuro dark:placeholder-white p-2 border-2 border-azul rounded-xl placeholder:text-sm"
+              required
+              autoComplete="off"
+            />
+          </div>
+          <div className="flex flex-col gap-1">
+            <label htmlFor="email" className="font-bold ml-1">
+              E-mail
+            </label>
+            <input
+              type="text"
+              placeholder="Digite seu e-mail..."
+              className="placeholder-black focus:outline-none dark:bg-cinzaescuro dark:placeholder-white p-2 border-2 border-azul rounded-xl placeholder:text-sm"
+              required
+              autoComplete="off"
+            />
+          </div>
+          <div className="flex flex-col gap-1">
+            <label htmlFor="senha" className="font-bold ml-1">
+              Senha:
+            </label>
+            <input
+              type="text"
+              placeholder="Digite sua senha..."
+              className="placeholder-black focus:outline-none dark:bg-cinzaescuro dark:placeholder-white p-2 border-2 border-azul rounded-xl placeholder:text-sm"
+              required
+              autoComplete="off"
+            />
+          </div>
+          <div className="flex flex-col items-center mt-2">
+            <button className="pr-6 pl-6 pt-2 pb-2 bg-azul font-bold text-2xl rounded-xl text-white">
+              Criar conta
+            </button>
+          </div>
+        </form>
+        <div className="w-full h-[2rem] flex justify-center items-center ">
+          <p>
+            © Copyright 2024 by Phygital Porto. Todos os direitos reservados.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
--- sprint4/src/app/login/page.tsx
+++ sprint4/src/app/login/page.tsx
@@ -1,5 +1,5 @@
 "use client";
- 
+
 import { IoMdCloseCircleOutline } from "react-icons/io";
 import Image from "next/image";
 import ThemeToggle from "@/components/ThemeToggle";
@@ -7,13 +7,13 @@ import { useState } from "react";
 import { HiOutlineMail } from "react-icons/hi";
 import { TbLock } from "react-icons/tb";
 import Link from "next/link";
- 
+
 export default function Login() {
   const [isDarkMode, setIsDarkMode] = useState(false);
   const handleThemeToggle = (darkMode: boolean) => {
     setIsDarkMode(darkMode);
   };
- 
+
   return (
     <div className="flex w-full h-[100vh] justify-evenly items-center transition duration-300 dark:bg-cinzaescuro">
       <div className="flex gap-2">
@@ -33,9 +33,11 @@ export default function Login() {
       <div className="w-[35rem] h-[40rem] bg-cinza flex flex-col border-4 rounded-xl border-azul dark:bg-preto">
         <div className="flex w-full h-[3rem] justify-end items-center pr-4 gap-2 text-azul">
           <ThemeToggle onToggle={handleThemeToggle} />
-          <IoMdCloseCircleOutline className="text-[1.7rem]" />
+          <Link href="/">
+            <IoMdCloseCircleOutline className="text-[1.7rem] hover:text-red-500 transtion duration-300" />
+          </Link>
         </div>
-        <div className="w-full h-[35rem] flex flex-col gap-4 justify-center items-center">
+        <form className="w-full h-[35rem] flex flex-col gap-4 justify-center items-center">
           <h1 className="font-bold text-3xl">Login</h1>
           <p className="text">Por favor, faça login na sua conta!</p>
           <div className="flex bg-white p-2 items-center border-2 border-azul rounded-xl dark:bg-cinzaescuro">
@@ -43,22 +45,33 @@ export default function Login() {
               type="text"
               placeholder="E-mail"
               className="font-bold placeholder-black focus:outline-none dark:bg-cinzaescuro dark:placeholder-white"
+              required
+              autoComplete="off"
             />
             <HiOutlineMail />
           </div>
-          <div className="flex bg-white p-2 items-center border-2 border-azul rounded-xl">
+          <div className="flex bg-white p-2 items-center border-2 border-azul rounded-xl dark:bg-cinzaescuro">
             <input
               type="password"
               placeholder="Senha"
               className="font-bold placeholder-black focus:outline-none dark:bg-cinzaescuro dark:placeholder-white"
+              required
+              autoComplete="off"
             />
             <TbLock />
           </div>
           <div className="flex flex-col items-center gap-1">
-            <button className="pr-6 pl-6 pt-2 pb-2 bg-azul font-bold text-2xl rounded-xl text-white">Entrar</button>
-            <Link className="text-[13px] underline transition duration-300 hover:text-azul" href="/">Não possui conta? Crie uma agora!</Link>
+            <button className="pr-6 pl-6 pt-2 pb-2 bg-azul font-bold text-2xl rounded-xl text-white">
+              Entrar
+            </button>
+            <Link
+              className="text-[13px] underline transition duration-300 hover:text-azul"
+              href="/cadastro"
+            >
+              Não possui conta? Crie uma agora!
+            </Link>
           </div>
-        </div>
+        </form>
         <div className="w-full h-[2rem] flex justify-center items-center ">
           <p>
             © Copyright 2024 by Phygital Porto. Todos os direitos reservados.
